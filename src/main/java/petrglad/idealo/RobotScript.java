cc014eb05d7367a3ba16f6fedee2c1a119e92d7e
Petr Gladkikh <petrglad@gmail.com>
2024-05-20 11:02:01 +0200

    Cleanup: extract a function

--- src/main/java/petrglad/idealo/RobotScript.java
+++ src/main/java/petrglad/idealo/RobotScript.java
@@ -5,6 +5,11 @@ import com.google.common.base.Splitter;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Robot script interpreter.
+ * X coordinate raises from West to East.
+ * Y coordinate raises from North to South.
+ */
 public class RobotScript {
 
     static List<String> parseScriptLine(String line) {
@@ -29,47 +34,42 @@ public class RobotScript {
     }
 
     Robot processCommand(Robot robot, List<String> tokens) {
-        switch (parseCommandId(tokens.get(0))) {
-            case POSITION: {
-                return new Robot(
-                        Integer.parseInt(tokens.get(1)),
-                        Integer.parseInt(tokens.get(2)),
-                        parseDirection(tokens.get(3)));
+        return switch (parseCommandId(tokens.get(0))) {
+            case POSITION -> new Robot(
+                    Integer.parseInt(tokens.get(1)),
+                    Integer.parseInt(tokens.get(2)),
+                    parseDirection(tokens.get(3)));
+            case WAIT -> robot;
+            case FORWARD -> moveForward(robot, tokens);
+            case RIGHT -> new Robot(robot.x, robot.y, robot.direction.turnRight());
+            case LEFT -> new Robot(robot.x, robot.y, robot.direction.turnLeft());
+            case TURNAROUND -> new Robot(robot.x, robot.y, robot.direction.turnBack());
+            default -> throw new IllegalArgumentException("Unsupported command: " + tokens.get(0));
+        };
+    }
+
+    private static Robot moveForward(Robot robot, List<String> tokens) {
+        int steps = Integer.parseInt(tokens.get(1));
+        int x = robot.x;
+        int y = robot.y;
+        switch (robot.direction) {
+            case EAST: {
+                x += steps;
+                break;
             }
-            case WAIT:
-                return robot;
-            case FORWARD: {
-                int steps = Integer.parseInt(tokens.get(1));
-                int x = robot.x;
-                int y = robot.y;
-                switch (robot.direction) {
-                    case EAST: {
-                        x += steps;
-                        break;
-                    }
-                    case WEST: {
-                        x -= steps;
-                        break;
-                    }
-                    case SOUTH: {
-                        y += steps;
-                        break;
-                    }
-                    case NORTH: {
-                        y -= steps;
-                    }
-                }
-                return new Robot(x, y, robot.direction);
+            case WEST: {
+                x -= steps;
+                break;
             }
-            case RIGHT:
-                return new Robot(robot.x, robot.y, robot.direction.turnRight());
-            case LEFT:
-                return new Robot(robot.x, robot.y, robot.direction.turnLeft());
-            case TURNAROUND:
-                return new Robot(robot.x, robot.y, robot.direction.turnBack());
-            default:
-                throw new IllegalArgumentException("Unexpected command name: " + tokens.get(0));
+            case SOUTH: {
+                y += steps;
+                break;
+            }
+            case NORTH: {
+                y -= steps;
+            }
         }
+        return new Robot(x, y, robot.direction);
     }
 
     public Robot run(String script) {
